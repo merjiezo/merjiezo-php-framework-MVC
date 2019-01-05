c9aaa7240b9dc8ff3ce082727176ec4cc58bcfe1
merjiezo <merjiezo@gmail.com>
2019-01-05 11:41:51 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,14 +2,14 @@
 return [
 	'dsn'      => 'mysql:host=127.0.0.1;dbname=store',
 	'user'     => 'root',
-	'password' => '950826',
+	'password' => '01',
 	'charset'  => 'utf8',
 
 	'slaves' => [
 		[
 			'dsn'      => 'mysql:host=127.0.0.1;dbname=mjz',
 			'user'     => 'root',
-			'password' => '950826',
+			'password' => '01',
 			'charset'  => 'utf8',
 		],
 	],
